4499ff8c1ce777168dab8cc551a03039596aad76
Pauli Matila <pauli.matila@outlook.com>
2020-05-28 09:19:35 +0300

    Updates

---
+++ precache-manifest.784c1067a69834d8e8b29693cb129ada.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8cc66ba48b897af93b12ed96255b66d",
+    "url": "/stampcollector/index.html"
+  },
+  {
+    "revision": "3abf3d7512647f74d9ed",
+    "url": "/stampcollector/static/css/main.deae0c08.chunk.css"
+  },
+  {
+    "revision": "f92acd8020c881b73125",
+    "url": "/stampcollector/static/js/2.5df3ddfc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/stampcollector/static/js/2.5df3ddfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3abf3d7512647f74d9ed",
+    "url": "/stampcollector/static/js/main.5fb2e8dd.chunk.js"
+  },
+  {
+    "revision": "bca2795434ca61c9c36a",
+    "url": "/stampcollector/static/js/runtime-main.271f1afe.js"
+  }
+]);
